4f3ccafeaac378ae8b9a8696fe07f2dd7099794c
Charlie Tran <hi@charlietran.com>
2014-08-31 01:03:27 -0400

    add grid

--- js/main.js
+++ js/main.js
@@ -7,9 +7,36 @@ NF.options = {
 NF.elements = {
   desktop:                  $('#desktop'),
   background_chooser:       $('#background-chooser'),
-  background_chooser_links: $('#background-chooser > li > a')
+  background_chooser_links: $('#background-chooser > li > a'),
+  runner_grid:              $('#runner-grid'),
+  corp_grid:                $('#corp-grid')
 };
 
+NF.grids = {
+  options: {
+    widget_selector: '.card',
+    widget_base_dimensions: [150, 209],
+    autogrow_cols: true,
+    resize: { enabled: true }
+  }
+};
+
+NF.populateCards = function() {
+  $.getJSON('cards.json', function(card_data){
+    NF.Cards = TAFFY(card_data);
+  });
+};
+
+NF.setupGrid = function(self) {
+  self.grids.runner = self.elements.runner_grid.gridster(self.grids.options).data('gridster');
+  self.grids.corp = self.elements.corp_grid.gridster(self.grids.options).data('gridster');
+};
+
+NF.setupTemplates = function(self) {
+  self.templates = self.templates || {};
+  self.templates.card = $("#card-template").html();
+  Mustache.parse(self.templates.card);
+};
 
 NF.setupBgChooser = function() {
   var self = this;
@@ -23,24 +50,24 @@ NF.changeBg = function(el, self) {
   console.log($this.data('url'));
 };
 
-NF.addCard = function(card) {
-  var self = this
-  var template = $("#card-template").html();
-  Mustache.parse(template);
-  var context = {image_url: self.options.image_host + card.imagesrc}
-  var card_html = Mustache.render(template, context);
-
-  self.elements.desktop.append(card_html);
+NF.addCard = function(card, side) {
+  var self = this;
+  var card_data = {
+    image_url: self.options.image_host + card.imagesrc,
+    card_title: card.title,
+    card_text: card.text.replace("\n", "<br><br>")
+  };
+  var card_html = Mustache.render(self.templates.card, card_data);
+  self.grids[side].add_widget(card_html, 1, 1);
+  self.elements.desktop.foundation();
 };
 
-NF.populateCards = function() {
-  $.getJSON('cards.json', function(card_data){
-    NF.Cards = TAFFY(card_data);
-  });
-};
 
 NF.ready = function() {
+  var self = this;
   NF.populateCards();
+  NF.setupGrid(self);
+  NF.setupTemplates(self);
   NF.setupBgChooser();
 };
 
